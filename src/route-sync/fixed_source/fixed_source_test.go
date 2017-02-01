02dad4f74db559767e1084ec0dcc3a898efe94b3
Jeff Johnson <jrjohnson@google.com>
2017-02-01 23:07:14 +0000

    Use string for ip instead of net.IP
    [#137357741]

--- src/route-sync/fixed_source/fixed_source_test.go
+++ src/route-sync/fixed_source/fixed_source_test.go
@@ -1,7 +1,6 @@
 package fixed_source_test
 
 import (
-	"net"
 	"route-sync/fixed_source"
 	"route-sync/route"
 
@@ -20,8 +19,8 @@ var _ = Describe("FixedSource", func() {
 	})
 
 	It("returns TCP routes", func() {
-		routes := []*route.TCP{&route.TCP{Frontend: route.Endpoint{IP: net.ParseIP("10.0.0.1"), Port: 8080},
-			Backend: []route.Endpoint{route.Endpoint{IP: net.ParseIP("10.10.10.10"), Port: 9090}},
+		routes := []*route.TCP{&route.TCP{Frontend: route.Endpoint{IP: "10.0.0.1", Port: 8080},
+			Backend: []route.Endpoint{route.Endpoint{IP: "10.10.10.10", Port: 9090}},
 		}}
 
 		fs := fixed_source.New(routes)
