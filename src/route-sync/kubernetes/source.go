a3ce435b66dbed1f7d1310132840d909bda5502f
Meaghan Kjelland <mkjelland@google.com>
2017-02-02 17:54:34 +0000

    Handle case where we have multiple ports for a service
    [#137357741]

--- src/route-sync/kubernetes/source.go
+++ src/route-sync/kubernetes/source.go
@@ -2,8 +2,9 @@ package kubernetes
 
 import (
 	"route-sync/route"
-        "k8s.io/client-go/pkg/api/v1"
+
 	k8s "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/api/v1"
 )
 
 type endpoint struct {
@@ -17,41 +18,44 @@ func New(clientset k8s.Interface) route.Source {
 func (e *endpoint) TCP() ([]*route.TCP, error) {
 	nodes, err := e.clientset.CoreV1().Nodes().List(v1.ListOptions{})
 
-        if err != nil {
-                panic(err)
-        }
+	if err != nil {
+		panic(err)
+	}
 
-        services, err := e.clientset.CoreV1().Services("").List(v1.ListOptions{})
+	services, err := e.clientset.CoreV1().Services("").List(v1.ListOptions{})
 
-        if err != nil {
-                panic(err)
-        }
+	if err != nil {
+		panic(err)
+	}
 
 	ips := []string{}
 	for _, node := range nodes.Items {
 		for _, address := range node.Status.Addresses {
-                        if address.Type == "InternalIP" {
+			if address.Type == "InternalIP" {
 				ips = append(ips, address.Address)
-                        }
-                }
+			}
+		}
 	}
 
 	routes := []*route.TCP{}
-        for _, service := range services.Items {
-		if len(service.Spec.Ports) != 1 {
-			continue
-		}
-		var nodePort int = int(service.Spec.Ports[0].NodePort)
-		frontend := nodePort
+	for _, service := range services.Items {
+		for _, port := range service.Spec.Ports {
+			var nodePort int = int(port.NodePort)
+			if port.Protocol == "UDP" {
+				continue
+			}
+			if nodePort <= 0 {
+				continue
+			}
+			backends := []route.Endpoint{}
+			for _, ip := range ips {
+				backends = append(backends, route.Endpoint{IP: ip, Port: nodePort})
+			}
 
-		backends := []route.Endpoint{}
-		for _, ip := range ips {
-			backends = append(backends, route.Endpoint{IP: ip, Port: nodePort})
+			tcp := &route.TCP{Frontend: nodePort, Backend: backends}
+			routes = append(routes, tcp)
 		}
-
-		tcp := &route.TCP{Frontend: frontend, Backend: backends}
-		routes = append(routes, tcp)
-        }
+	}
 	return routes, nil
 }
 
--- src/route-sync/kubernetes/source_test.go
+++ src/route-sync/kubernetes/source_test.go
@@ -47,5 +47,126 @@ var _ = Describe("Source", func() {
 			Expect(backends[0].Port).To(Equal(nodePort))
 			Expect(tcpRoute.Backend[0].IP).To(Equal(nodeAddress))
 		})
+		It("returns list of TCP routes", func() {
+			nodeAddress := "10.0.0.0"
+			nodePort := 42
+			clientset := fake.NewSimpleClientset()
+			clientset.PrependReactor("list", "services", func(action core.Action) (bool, runtime.Object, error) {
+				port := v1.ServicePort{NodePort: int32(nodePort)}
+				ports := []v1.ServicePort{port}
+				spec := v1.ServiceSpec{Ports: ports}
+				s1 := v1.Service{Spec: spec}
+				s2 := v1.Service{Spec: spec}
+				serviceList := []v1.Service{s1, s2}
+				return true, &v1.ServiceList{Items: serviceList}, nil
+			})
+			clientset.PrependReactor("list", "nodes", func(action core.Action) (bool, runtime.Object, error) {
+				address := v1.NodeAddress{Type: "InternalIP", Address: nodeAddress}
+				addresses := []v1.NodeAddress{address}
+				ns := v1.NodeStatus{Addresses: addresses}
+				n1 := v1.Node{Status: ns}
+				n2 := v1.Node{Status: ns}
+				nodesList := []v1.Node{n1, n2}
+				return true, &v1.NodeList{Items: nodesList}, nil
+			})
+
+			endpoint := kubernetes.New(clientset)
+			routes, err := endpoint.TCP()
+			Expect(err).To(BeNil())
+			Expect(len(routes)).To(Equal(2))
+			tcpRoute := routes[0]
+			Expect(tcpRoute.Frontend).To(Equal(nodePort))
+			backends := tcpRoute.Backend
+			Expect(len(backends)).To(Equal(2))
+			Expect(backends[0].Port).To(Equal(nodePort))
+			Expect(tcpRoute.Backend[0].IP).To(Equal(nodeAddress))
+		})
+		It("returns empty list of TCP routes when there are no services", func() {
+			clientset := fake.NewSimpleClientset()
+			clientset.PrependReactor("list", "services", func(action core.Action) (bool, runtime.Object, error) {
+				serviceList := []v1.Service{}
+				return true, &v1.ServiceList{Items: serviceList}, nil
+			})
+			clientset.PrependReactor("list", "nodes", func(action core.Action) (bool, runtime.Object, error) {
+				nodesList := []v1.Node{}
+				return true, &v1.NodeList{Items: nodesList}, nil
+			})
+
+			endpoint := kubernetes.New(clientset)
+			routes, err := endpoint.TCP()
+			Expect(err).To(BeNil())
+			Expect(len(routes)).To(Equal(0))
+		})
+		It("returns only TCP routes for services with multiple ports", func() {
+			nodeAddress := "10.0.0.0"
+			udpNodePort := 42
+			tcpNodePort := 43
+			anotherTCPNodePort := 4342
+			clientset := fake.NewSimpleClientset()
+			clientset.PrependReactor("list", "services", func(action core.Action) (bool, runtime.Object, error) {
+				port1 := v1.ServicePort{Protocol: "UDP", NodePort: int32(udpNodePort)}
+				port2 := v1.ServicePort{Protocol: "TCP", NodePort: int32(tcpNodePort)}
+				port3 := v1.ServicePort{Protocol: "TCP", NodePort: int32(anotherTCPNodePort)}
+				ports := []v1.ServicePort{port1, port2, port3}
+				spec := v1.ServiceSpec{Ports: ports}
+				s1 := v1.Service{Spec: spec}
+				serviceList := []v1.Service{s1}
+				return true, &v1.ServiceList{Items: serviceList}, nil
+			})
+			clientset.PrependReactor("list", "nodes", func(action core.Action) (bool, runtime.Object, error) {
+				address := v1.NodeAddress{Type: "InternalIP", Address: nodeAddress}
+				addresses := []v1.NodeAddress{address}
+				ns := v1.NodeStatus{Addresses: addresses}
+				n1 := v1.Node{Status: ns}
+				nodesList := []v1.Node{n1}
+				return true, &v1.NodeList{Items: nodesList}, nil
+			})
+
+			endpoint := kubernetes.New(clientset)
+			routes, err := endpoint.TCP()
+			Expect(err).To(BeNil())
+			Expect(len(routes)).To(Equal(2))
+			tcpRoute := routes[0]
+			Expect(tcpRoute.Frontend).To(Equal(tcpNodePort))
+			backends := tcpRoute.Backend
+			Expect(len(backends)).To(Equal(1))
+			Expect(backends[0].Port).To(Equal(tcpNodePort))
+			Expect(tcpRoute.Backend[0].IP).To(Equal(nodeAddress))
+			Expect(routes[1].Backend[0].Port).To(Equal(anotherTCPNodePort))
+		})
+		It("skip routes for services with no NodePort", func() {
+			nodeAddress := "10.0.0.0"
+			nodePort := 42
+			clientset := fake.NewSimpleClientset()
+			clientset.PrependReactor("list", "services", func(action core.Action) (bool, runtime.Object, error) {
+				port1 := v1.ServicePort{Protocol: "TCP", NodePort: int32(nodePort)}
+				port2 := v1.ServicePort{Protocol: "TCP"}
+				ports := []v1.ServicePort{port1, port2}
+				spec := v1.ServiceSpec{Ports: ports}
+				s1 := v1.Service{Spec: spec}
+				serviceList := []v1.Service{s1}
+				return true, &v1.ServiceList{Items: serviceList}, nil
+			})
+			clientset.PrependReactor("list", "nodes", func(action core.Action) (bool, runtime.Object, error) {
+				address := v1.NodeAddress{Type: "InternalIP", Address: nodeAddress}
+				addresses := []v1.NodeAddress{address}
+				ns := v1.NodeStatus{Addresses: addresses}
+				n1 := v1.Node{Status: ns}
+				nodesList := []v1.Node{n1}
+				return true, &v1.NodeList{Items: nodesList}, nil
+			})
+
+			endpoint := kubernetes.New(clientset)
+			routes, err := endpoint.TCP()
+			Expect(err).To(BeNil())
+			Expect(len(routes)).To(Equal(1))
+			tcpRoute := routes[0]
+			Expect(tcpRoute.Frontend).To(Equal(nodePort))
+			backends := tcpRoute.Backend
+			Expect(len(backends)).To(Equal(1))
+			Expect(backends[0].Port).To(Equal(nodePort))
+			Expect(tcpRoute.Backend[0].IP).To(Equal(nodeAddress))
+		})
+
 	})
 })
