a80146493e87fac8f6f16968092a9bd10fc85485
Meaghan Kjelland <mkjelland@google.com>
2017-02-02 23:37:46 +0000

    WIP: Initial method for getting HTTP routes [#137357741]

--- src/route-sync/kubernetes/source.go
+++ src/route-sync/kubernetes/source.go
@@ -17,17 +17,83 @@ func New(clientset k8s.Interface) route.Source {
 
 func (e *endpoint) TCP() ([]*route.TCP, error) {
 	nodes, err := e.clientset.CoreV1().Nodes().List(v1.ListOptions{})
-
 	if err != nil {
 		panic(err)
 	}
 
-	services, err := e.clientset.CoreV1().Services("").List(v1.ListOptions{})
-
+	namespaces, err := e.clientset.CoreV1().Namespaces().List(v1.ListOptions{})
 	if err != nil {
 		panic(err)
 	}
 
+	routes := []*route.TCP{}
+	for _, namespace := range namespaces.Items {
+		services, err := e.clientset.CoreV1().Services(namespace.ObjectMeta.GetName()).List(v1.ListOptions{})
+		if err != nil {
+			panic(err)
+		}
+
+		ips, _ := GetIPs(nodes)
+
+		for _, service := range services.Items {
+			for _, port := range service.Spec.Ports {
+				var nodePort int = int(port.NodePort)
+				if port.Protocol == "UDP" {
+					continue
+				}
+				if nodePort <= 0 {
+					continue
+				}
+				backends, _ := GetBackends(ips, nodePort)
+				tcp := &route.TCP{Frontend: nodePort, Backend: backends}
+				routes = append(routes, tcp)
+			}
+		}
+	}
+	return routes, nil
+}
+
+func (e *endpoint) HTTP() ([]*route.HTTP, error) {
+	nodes, err := e.clientset.CoreV1().Nodes().List(v1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	namespaces, err := e.clientset.CoreV1().Namespaces().List(v1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	routes := []*route.HTTP{}
+	for _, namespace := range namespaces.Items {
+		services, err := e.clientset.CoreV1().Services(namespace.ObjectMeta.GetName()).List(v1.ListOptions{})
+		if err != nil {
+			panic(err)
+		}
+
+		ips, _ := GetIPs(nodes)
+
+		for _, service := range services.Items {
+			for _, port := range service.Spec.Ports {
+				// TODO Which ports should we include for HTTP routing?
+				var nodePort int = int(port.NodePort)
+				if port.Protocol == "UDP" {
+					continue
+				}
+				if nodePort <= 0 {
+					continue
+				}
+				backends, _ := GetBackends(ips, nodePort)
+				// TODO Append a CF domain onto the Name param
+				http := &route.HTTP{Name: service.ObjectMeta.GetName() + "." + namespace.ObjectMeta.GetName(), Backend: backends}
+				routes = append(routes, http)
+			}
+		}
+	}
+	return routes, nil
+}
+
+func GetIPs(nodes *v1.NodeList) ([]string, error) {
 	ips := []string{}
 	for _, node := range nodes.Items {
 		for _, address := range node.Status.Addresses {
@@ -36,29 +102,13 @@ func (e *endpoint) TCP() ([]*route.TCP, error) {
 			}
 		}
 	}
-
-	routes := []*route.TCP{}
-	for _, service := range services.Items {
-		for _, port := range service.Spec.Ports {
-			var nodePort int = int(port.NodePort)
-			if port.Protocol == "UDP" {
-				continue
-			}
-			if nodePort <= 0 {
-				continue
-			}
-			backends := []route.Endpoint{}
-			for _, ip := range ips {
-				backends = append(backends, route.Endpoint{IP: ip, Port: nodePort})
-			}
-
-			tcp := &route.TCP{Frontend: nodePort, Backend: backends}
-			routes = append(routes, tcp)
-		}
-	}
-	return routes, nil
+	return ips, nil
 }
 
-func (e *endpoint) HTTP() ([]*route.HTTP, error) {
-	return []*route.HTTP{}, nil
+func GetBackends(ips []string, nodePort int) ([]route.Endpoint, error) {
+	backends := []route.Endpoint{}
+	for _, ip := range ips {
+		backends = append(backends, route.Endpoint{IP: ip, Port: nodePort})
+	}
+	return backends, nil
 }
--- src/route-sync/kubernetes/source_test.go
+++ src/route-sync/kubernetes/source_test.go
@@ -13,9 +13,97 @@ import (
 )
 
 var _ = Describe("Source", func() {
+	Context("HTTP", func() {
+		It("returns list of HTTP routes", func() {
+			namespace := "kubo-test"
+			nodeAddress := "10.0.0.0"
+			nodePort := 42
+			serviceName := "dashboard"
+			clientset := fake.NewSimpleClientset()
+			clientset.PrependReactor("list", "services", func(action core.Action) (bool, runtime.Object, error) {
+				port := v1.ServicePort{NodePort: int32(nodePort)}
+				ports := []v1.ServicePort{port}
+				spec := v1.ServiceSpec{Ports: ports}
+				objectMeta := v1.ObjectMeta{Name: serviceName}
+				s1 := v1.Service{Spec: spec, ObjectMeta: objectMeta}
+				serviceList := []v1.Service{s1}
+				return true, &v1.ServiceList{Items: serviceList}, nil
+			})
+			clientset.PrependReactor("list", "nodes", func(action core.Action) (bool, runtime.Object, error) {
+				address := v1.NodeAddress{Type: "InternalIP", Address: nodeAddress}
+				addresses := []v1.NodeAddress{address}
+				ns := v1.NodeStatus{Addresses: addresses}
+				n1 := v1.Node{Status: ns}
+				nodesList := []v1.Node{n1}
+				return true, &v1.NodeList{Items: nodesList}, nil
+			})
+			clientset.PrependReactor("list", "namespaces", func(action core.Action) (bool, runtime.Object, error) {
+				objectMeta := v1.ObjectMeta{Name: namespace}
+				ns1 := v1.Namespace{ObjectMeta: objectMeta}
+				namespaces := []v1.Namespace{ns1}
+				return true, &v1.NamespaceList{Items: namespaces}, nil
+			})
+			endpoint := kubernetes.New(clientset)
+			routes, err := endpoint.HTTP()
+			Expect(err).To(BeNil())
+			Expect(len(routes)).To(Equal(1))
+			httpRoute := routes[0]
+			Expect(httpRoute.Name).To(Equal(serviceName + "." + namespace))
+			backends := httpRoute.Backend
+			Expect(len(backends)).To(Equal(1))
+			Expect(backends[0].Port).To(Equal(nodePort))
+			Expect(httpRoute.Backend[0].IP).To(Equal(nodeAddress))
+		})
+		It("returns list of HTTP routes across namespaces", func() {
+			namespace := "kubo-test"
+			anotherNamespace := "default"
+			nodeAddress := "10.0.0.0"
+			nodePort := 42
+			serviceName := "dashboard"
+			clientset := fake.NewSimpleClientset()
+			clientset.PrependReactor("list", "services", func(action core.Action) (bool, runtime.Object, error) {
+				port := v1.ServicePort{NodePort: int32(nodePort)}
+				ports := []v1.ServicePort{port}
+				spec := v1.ServiceSpec{Ports: ports}
+				objectMeta := v1.ObjectMeta{Name: serviceName}
+				s1 := v1.Service{Spec: spec, ObjectMeta: objectMeta}
+				serviceList := []v1.Service{s1}
+				return true, &v1.ServiceList{Items: serviceList}, nil
+			})
+			clientset.PrependReactor("list", "nodes", func(action core.Action) (bool, runtime.Object, error) {
+				address := v1.NodeAddress{Type: "InternalIP", Address: nodeAddress}
+				addresses := []v1.NodeAddress{address}
+				ns := v1.NodeStatus{Addresses: addresses}
+				n1 := v1.Node{Status: ns}
+				nodesList := []v1.Node{n1}
+				return true, &v1.NodeList{Items: nodesList}, nil
+			})
+			clientset.PrependReactor("list", "namespaces", func(action core.Action) (bool, runtime.Object, error) {
+				objectMeta := v1.ObjectMeta{Name: namespace}
+				objectMeta2 := v1.ObjectMeta{Name: anotherNamespace}
+				ns1 := v1.Namespace{ObjectMeta: objectMeta}
+				ns2 := v1.Namespace{ObjectMeta: objectMeta2}
+				namespaces := []v1.Namespace{ns1, ns2}
+				return true, &v1.NamespaceList{Items: namespaces}, nil
+			})
+			endpoint := kubernetes.New(clientset)
+			routes, err := endpoint.HTTP()
+			Expect(err).To(BeNil())
+			Expect(len(routes)).To(Equal(2))
+			ns1Route := routes[0]
+			Expect(ns1Route.Name).To(Equal(serviceName + "." + namespace))
+			backends := ns1Route.Backend
+			Expect(len(backends)).To(Equal(1))
+			Expect(backends[0].Port).To(Equal(nodePort))
+			Expect(ns1Route.Backend[0].IP).To(Equal(nodeAddress))
+			ns2Route := routes[1]
+			Expect(ns2Route.Name).To(Equal(serviceName + "." + anotherNamespace))
+		})
+	})
 
 	Context("TCP", func() {
 		It("returns list of TCP routes", func() {
+			namespace := "kubo-test"
 			nodeAddress := "10.0.0.0"
 			nodePort := 42
 			clientset := fake.NewSimpleClientset()
@@ -35,6 +123,12 @@ var _ = Describe("Source", func() {
 				nodesList := []v1.Node{n1}
 				return true, &v1.NodeList{Items: nodesList}, nil
 			})
+			clientset.PrependReactor("list", "namespaces", func(action core.Action) (bool, runtime.Object, error) {
+				objectMeta := v1.ObjectMeta{Name: namespace}
+				ns1 := v1.Namespace{ObjectMeta: objectMeta}
+				namespaces := []v1.Namespace{ns1}
+				return true, &v1.NamespaceList{Items: namespaces}, nil
+			})
 
 			endpoint := kubernetes.New(clientset)
 			routes, err := endpoint.TCP()
@@ -48,6 +142,7 @@ var _ = Describe("Source", func() {
 			Expect(tcpRoute.Backend[0].IP).To(Equal(nodeAddress))
 		})
 		It("returns list of TCP routes", func() {
+			namespace := "kubo-test"
 			nodeAddress := "10.0.0.0"
 			nodePort := 42
 			clientset := fake.NewSimpleClientset()
@@ -68,6 +163,12 @@ var _ = Describe("Source", func() {
 				n2 := v1.Node{Status: ns}
 				nodesList := []v1.Node{n1, n2}
 				return true, &v1.NodeList{Items: nodesList}, nil
+			})
+			clientset.PrependReactor("list", "namespaces", func(action core.Action) (bool, runtime.Object, error) {
+				objectMeta := v1.ObjectMeta{Name: namespace}
+				ns1 := v1.Namespace{ObjectMeta: objectMeta}
+				namespaces := []v1.Namespace{ns1}
+				return true, &v1.NamespaceList{Items: namespaces}, nil
 			})
 
 			endpoint := kubernetes.New(clientset)
@@ -98,6 +199,7 @@ var _ = Describe("Source", func() {
 			Expect(len(routes)).To(Equal(0))
 		})
 		It("returns only TCP routes for services with multiple ports", func() {
+			namespace := "kubo-test"
 			nodeAddress := "10.0.0.0"
 			udpNodePort := 42
 			tcpNodePort := 43
@@ -121,6 +223,12 @@ var _ = Describe("Source", func() {
 				nodesList := []v1.Node{n1}
 				return true, &v1.NodeList{Items: nodesList}, nil
 			})
+			clientset.PrependReactor("list", "namespaces", func(action core.Action) (bool, runtime.Object, error) {
+				objectMeta := v1.ObjectMeta{Name: namespace}
+				ns1 := v1.Namespace{ObjectMeta: objectMeta}
+				namespaces := []v1.Namespace{ns1}
+				return true, &v1.NamespaceList{Items: namespaces}, nil
+			})
 
 			endpoint := kubernetes.New(clientset)
 			routes, err := endpoint.TCP()
@@ -135,6 +243,7 @@ var _ = Describe("Source", func() {
 			Expect(routes[1].Backend[0].Port).To(Equal(anotherTCPNodePort))
 		})
 		It("skip routes for services with no NodePort", func() {
+			namespace := "kubo-test"
 			nodeAddress := "10.0.0.0"
 			nodePort := 42
 			clientset := fake.NewSimpleClientset()
@@ -154,6 +263,12 @@ var _ = Describe("Source", func() {
 				n1 := v1.Node{Status: ns}
 				nodesList := []v1.Node{n1}
 				return true, &v1.NodeList{Items: nodesList}, nil
+			})
+			clientset.PrependReactor("list", "namespaces", func(action core.Action) (bool, runtime.Object, error) {
+				objectMeta := v1.ObjectMeta{Name: namespace}
+				ns1 := v1.Namespace{ObjectMeta: objectMeta}
+				namespaces := []v1.Namespace{ns1}
+				return true, &v1.NamespaceList{Items: namespaces}, nil
 			})
 
 			endpoint := kubernetes.New(clientset)
