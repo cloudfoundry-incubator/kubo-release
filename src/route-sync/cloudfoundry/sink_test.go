6e3995c12c5c61b0922d261fec66c01d1a325cd9
Jeff Johnson <jrjohnson@google.com>
2017-02-02 17:47:07 +0000

    Ensure n routes are posted
    [#137357741]

--- src/route-sync/cloudfoundry/sink_test.go
+++ src/route-sync/cloudfoundry/sink_test.go
@@ -13,19 +13,29 @@ import (
 var _ = Describe("Sink", func() {
 	Context("with a valid message bus", func() {
 		var (
-			sink route.Sink
-			bus  fakes.FakeMessageBus
+			sink       route.Sink
+			bus        fakes.FakeMessageBus
+			httpRoutes []*route.HTTP
 		)
 		BeforeEach(func() {
 			bus = fakes.FakeMessageBus{}
 			sink = NewSink(&bus)
+			httpRoutes = []*route.HTTP{
+				&route.HTTP{
+					Backend: []route.Endpoint{route.Endpoint{IP: "10.10.10.10", Port: 9090}},
+					Name:    "foobar.cf-app.com",
+				},
+				&route.HTTP{
+					Backend: []route.Endpoint{
+						route.Endpoint{IP: "10.10.10.10", Port: 9090},
+						route.Endpoint{IP: "10.2.2.2", Port: 8080},
+					},
+					Name: "baz.cf-app.com",
+				},
+			}
 		})
 		It("posts a single L7 route", func() {
-			httpRoute := route.HTTP{
-				Backend: []route.Endpoint{route.Endpoint{IP: "10.10.10.10", Port: 9090}},
-				Name:    "foobar.cf-app.com",
-			}
-			sink.HTTP([]*route.HTTP{&httpRoute})
+			sink.HTTP([]*route.HTTP{httpRoutes[0]})
 			Expect(bus.SendMessageCallCount()).To(Equal(1))
 			subject, host, route, privateInstanceId := bus.SendMessageArgsForCall(0)
 
@@ -35,6 +45,10 @@ var _ = Describe("Sink", func() {
 			Expect(route.URIs).To(HaveLen(1))
 			Expect(route.URIs[0]).To(Equal("foobar.cf-app.com"))
 			Expect(privateInstanceId).NotTo(Equal(""))
+		})
+		It("posts multiple L7 routes with multiple backends", func() {
+			sink.HTTP(httpRoutes)
+			Expect(bus.SendMessageCallCount()).To(Equal(3))
 		})
 	})
 })
