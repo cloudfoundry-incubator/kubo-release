ab2d62a9c4e115322039b9c01eb812d8500328c7
Jeff Johnson <jrjohnson@google.com>
2017-02-07 03:49:42 +0000

    Variable naming cleanup
    [#137358437]

--- src/route-sync/cloudfoundry/sink_test.go
+++ src/route-sync/cloudfoundry/sink_test.go
@@ -4,7 +4,7 @@ import (
 	. "route-sync/cloudfoundry"
 	"route-sync/route"
 
-	"code.cloudfoundry.org/route-registrar/messagebus/fakes"
+	messagebusfakes "code.cloudfoundry.org/route-registrar/messagebus/fakes"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -14,12 +14,12 @@ var _ = Describe("Sink", func() {
 	Context("with a valid message bus", func() {
 		var (
 			sink       route.Sink
-			bus        fakes.FakeMessageBus
+			msgBus     messagebusfakes.FakeMessageBus
 			httpRoutes []*route.HTTP
 		)
 		BeforeEach(func() {
-			bus = fakes.FakeMessageBus{}
-			sink = NewSink(&bus)
+			msgBus = messagebusfakes.FakeMessageBus{}
+			sink = NewSink(&msgBus)
 			httpRoutes = []*route.HTTP{
 				&route.HTTP{
 					Backend: []route.Endpoint{route.Endpoint{IP: "10.10.10.10", Port: 9090}},
@@ -36,8 +36,8 @@ var _ = Describe("Sink", func() {
 		})
 		It("posts a single L7 route", func() {
 			sink.HTTP([]*route.HTTP{httpRoutes[0]})
-			Expect(bus.SendMessageCallCount()).To(Equal(1))
-			subject, host, route, privateInstanceId := bus.SendMessageArgsForCall(0)
+			Expect(msgBus.SendMessageCallCount()).To(Equal(1))
+			subject, host, route, privateInstanceId := msgBus.SendMessageArgsForCall(0)
 
 			Expect(subject).To(Equal("router.register"))
 			Expect(host).To(Equal("10.10.10.10"))
@@ -48,7 +48,10 @@ var _ = Describe("Sink", func() {
 		})
 		It("posts multiple L7 routes with multiple backends", func() {
 			sink.HTTP(httpRoutes)
-			Expect(bus.SendMessageCallCount()).To(Equal(3))
+			Expect(msgBus.SendMessageCallCount()).To(Equal(3))
+		})
+		It("posts a sinlge L4 route", func() {
+
 		})
 	})
 })
