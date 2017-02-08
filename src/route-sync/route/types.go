ecf5bd918d72680a8472b2e9f449d26a4cf54fdf
Jeff Johnson <jrjohnson@google.com>
2017-02-08 00:17:19 +0000

    golint: route
    route/types.go:3:6: exported type Endpoint should have comment or be unexported
    route/types.go:8:6: exported type TCP should have comment or be unexported
    route/types.go:13:6: exported type HTTP should have comment or be unexported
    route/types.go:18:6: exported type Source should have comment or be unexported
    route/types.go:23:6: exported type Sink should have comment or be unexported

--- src/route-sync/route/types.go
+++ src/route-sync/route/types.go
@@ -1,25 +1,30 @@
 package route
 
+// Endpoint defines a given TCP Endpoint
 type Endpoint struct {
 	IP   string
 	Port int
 }
 
+// TCP is a route definition for TCP traffic
 type TCP struct {
 	Frontend int // This is a port
 	Backend  []Endpoint
 }
 
+// HTTP is a route definition for HTTP traffic
 type HTTP struct {
 	Name    string
 	Backend []Endpoint
 }
 
+// Source provides routes
 type Source interface {
 	TCP() ([]*TCP, error)
 	HTTP() ([]*HTTP, error)
 }
 
+// Sink consumes routes
 type Sink interface {
 	TCP(routes []*TCP) error
 	HTTP(routes []*HTTP) error
