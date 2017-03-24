4c8dfa991f23762f94899063b906ab4280a9991f
Meaghan Kjelland <mkjelland@google.com>
2017-03-24 18:08:30 +0000

    Produce UAA configuration
    [#140681955]
    
    Signed-off-by: Konstantin Semenov <ksemenov@pivotal.io>

--- src/route-sync/config/config.go
+++ src/route-sync/config/config.go
@@ -2,10 +2,11 @@ package config
 
 import (
 	"encoding/json"
-	"fmt"
 
 	cfConfig "code.cloudfoundry.org/route-registrar/config"
+	"errors"
 	"github.com/kelseyhightower/envconfig"
+	uaaconfig "code.cloudfoundry.org/uaa-go-client/config"
 )
 
 // Config contains settings for the route-sync with envconfig annotations
@@ -21,9 +22,9 @@ type Config struct {
 	KubeConfigPath            string                      `envconfig:"kube_config_path" required:"true"`
 }
 
-// NewConfig creates a Config object from the systems enviornment variables
+// NewConfig creates a Config object from the systems environment variables
 //
-// Pass in the values through enviornment variables with the ROUTESYNC_ prefix.
+// Pass in the values through environment variables with the ROUTESYNC_ prefix.
 // Eg: ROUTESYNC_CLOUD_FOUNDRY_API_URL="http://api.cf.example.org"
 func NewConfig() (*Config, error) {
 	cfg := Config{}
@@ -43,9 +44,18 @@ func NewConfig() (*Config, error) {
 	return &cfg, nil
 }
 
+func (cfg *Config) UAAConfig() *uaaconfig.Config {
+	return &uaaconfig.Config{
+		ClientName:       cfg.RoutingAPIUsername,
+		ClientSecret:     cfg.RoutingAPIClientSecret,
+		UaaEndpoint:      cfg.UAAApiURL,
+		SkipVerification: cfg.SkipTLSVerification,
+	}
+}
+
 func (cfg *Config) validate() error {
 	if len(cfg.NatsServers) == 0 {
-		return fmt.Errorf("no NatsServers specified in config")
+		return errors.New("no NATS servers specified in config")
 	}
 	return nil
 }
