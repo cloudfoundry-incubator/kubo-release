73ebe3f394818e26c18d23762bc7aed4e95e8ef6
Jeff Johnson <jrjohnson@google.com>
2017-03-29 17:46:54 +0100

    Refined application tests
    [#140681955]
    
    Signed-off-by: Konstantin Semenov <ksemenov@pivotal.io>

--- src/route-sync/application/application_test.go
+++ src/route-sync/application/application_test.go
@@ -4,6 +4,7 @@ import (
 	"route-sync/config"
 	"route-sync/pooler/poolerfakes"
 	"route-sync/route/routefakes"
+	cfConfig "code.cloudfoundry.org/route-registrar/config"
 
 	"code.cloudfoundry.org/lager"
 	"code.cloudfoundry.org/lager/lagertest"
@@ -16,10 +17,11 @@ var _ = Describe("Application", func() {
 	var (
 		logger lager.Logger
 		pooler *poolerfakes.FakePooler
-		src    *routefakes.FakeSource
+		source *routefakes.FakeSource
 		router *routefakes.FakeRouter
-		cfg    = &config.Config{
+		cfg        = &config.Config{
 			RawNatsServers:            "[{\"Host\": \"10.0.1.8:4222\",\"User\": \"nats\", \"Password\": \"natspass\"}]",
+			NatsServers:               []cfConfig.MessageBusServer{{Host: "host", User: "user", Password: "password"}},
 			RoutingApiUrl:             "https://api.cf.example.org",
 			CloudFoundryAppDomainName: "apps.cf.example.org",
 			UAAApiURL:                 "https://uaa.cf.example.org",
@@ -32,27 +34,41 @@ var _ = Describe("Application", func() {
 	BeforeEach(func() {
 		logger = lagertest.NewTestLogger("")
 		pooler = &poolerfakes.FakePooler{}
-		src = &routefakes.FakeSource{}
+		source = &routefakes.FakeSource{}
 		router = &routefakes.FakeRouter{}
 	})
 	Context("Application", func() {
 		It("runs the Application and eventually finishes", func() {
-			app := NewApplication(logger, pooler, src, router)
+			app := NewApplication(logger, pooler, source, router)
 			app.Run(0, cfg)
 
 			Eventually(logger).Should(gbytes.Say("exiting"))
 		})
+
 		It("starts the pooler", func() {
-			app := NewApplication(logger, pooler, src, router)
+			app := NewApplication(logger, pooler, source, router)
 			app.Run(0, cfg)
 
 			Expect(pooler.StartCallCount()).To(Equal(1))
+
+			startSource, startRouter := pooler.StartArgsForCall(0)
+			Expect(startSource).To(BeEquivalentTo(source))
+			Expect(startRouter).To(BeEquivalentTo(router))
 		})
+
 		It("starts a connection to nats", func() {
-			app := NewApplication(logger, pooler, src, router)
+			app := NewApplication(logger, pooler, source, router)
 			app.Run(0, cfg)
 
 			Expect(router.ConnectCallCount()).To(Equal(1))
+			messageBusServers, log := router.ConnectArgsForCall(0)
+
+			Expect(messageBusServers).To(HaveLen(1))
+			Expect(messageBusServers[0].Host).To(Equal("host"))
+			Expect(messageBusServers[0].User).To(Equal("user"))
+			Expect(messageBusServers[0].Password).To(Equal("password"))
+
+			Expect(log).To(BeEquivalentTo(logger))
 		})
 	})
 })
