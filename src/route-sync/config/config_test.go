82d584a9ea92fbd121ff150955e01d489efac947
Konstantin Semenov <ksemenov@pivotal.io>
2017-03-29 18:28:01 +0100

    Configuration creation without env vars set
    [#140681955]
    
    Signed-off-by: Jeff Johnson <jrjohnson@google.com>

--- src/route-sync/config/config_test.go
+++ src/route-sync/config/config_test.go
@@ -9,51 +9,71 @@ import (
 )
 
 var _ = Describe("Config", func() {
-	BeforeEach(func() {
-		os.Setenv("ROUTESYNC_NATS_SERVERS", "[{\"Host\": \"10.0.1.8:4222\",\"User\": \"nats\", \"Password\": \"natspass\"}]")
-		os.Setenv("ROUTESYNC_CLOUD_FOUNDRY_APP_DOMAIN_NAME", "apps.cf.example.org")
-		os.Setenv("ROUTESYNC_UAA_API_URL", "https://uaa.cf.example.org")
-		os.Setenv("ROUTESYNC_ROUTING_API_URL", "https://api.cf.example.org")
-		os.Setenv("ROUTESYNC_ROUTING_API_USERNAME", "routeUser")
-		os.Setenv("ROUTESYNC_ROUTING_API_CLIENT_SECRET", "aabbcc")
-		os.Setenv("ROUTESYNC_SKIP_TLS_VERIFICATION", "true")
-		os.Setenv("ROUTESYNC_KUBE_CONFIG_PATH", "~/.config/kube")
+	Context("Configuration is set", func() {
+
+		BeforeEach(func() {
+			os.Setenv("ROUTESYNC_NATS_SERVERS", "[{\"Host\": \"10.0.1.8:4222\",\"User\": \"nats\", \"Password\": \"natspass\"}]")
+			os.Setenv("ROUTESYNC_CLOUD_FOUNDRY_APP_DOMAIN_NAME", "apps.cf.example.org")
+			os.Setenv("ROUTESYNC_UAA_API_URL", "https://uaa.cf.example.org")
+			os.Setenv("ROUTESYNC_ROUTING_API_URL", "https://api.cf.example.org")
+			os.Setenv("ROUTESYNC_ROUTING_API_USERNAME", "routeUser")
+			os.Setenv("ROUTESYNC_ROUTING_API_CLIENT_SECRET", "aabbcc")
+			os.Setenv("ROUTESYNC_SKIP_TLS_VERIFICATION", "true")
+			os.Setenv("ROUTESYNC_KUBE_CONFIG_PATH", "~/.config/kube")
+		})
+
+		AfterEach(func() {
+			os.Unsetenv("ROUTESYNC_NATS_SERVERS")
+			os.Unsetenv("ROUTESYNC_CLOUD_FOUNDRY_APP_DOMAIN_NAME")
+			os.Unsetenv("ROUTESYNC_UAA_API_URL")
+			os.Unsetenv("ROUTESYNC_ROUTING_API_URL")
+			os.Unsetenv("ROUTESYNC_ROUTING_API_USERNAME")
+			os.Unsetenv("ROUTESYNC_ROUTING_API_CLIENT_SECRET")
+			os.Unsetenv("ROUTESYNC_SKIP_TLS_VERIFICATION")
+			os.Unsetenv("ROUTESYNC_KUBE_CONFIG_PATH")
+		})
+
+		It("returns a valid config frm the enviornment", func() {
+			c, err := NewConfig()
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(c.NatsServers).To(HaveLen(1))
+
+			natsServer := c.NatsServers[0]
+			Expect(natsServer.Host).To(Equal("10.0.1.8:4222"))
+			Expect(natsServer.User).To(Equal("nats"))
+			Expect(natsServer.Password).To(Equal("natspass"))
+		})
+
+		It("returns an error when nats server is not set", func() {
+			os.Setenv("ROUTESYNC_NATS_SERVERS", "[]")
+			cfg, err := NewConfig()
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("no NATS servers"))
+			Expect(cfg).To(BeNil())
+		})
+
+		It("returns an error when nats server is not a JSON object", func() {
+			os.Setenv("ROUTESYNC_NATS_SERVERS", "")
+			cfg, err := NewConfig()
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("JSON"))
+			Expect(cfg).To(BeNil())
+		})
+
+		It("Produces a UAA config", func() {
+			cfg, _ := NewConfig()
+			Expect(cfg.UAAConfig().ClientName).To(Equal("routeUser"))
+			Expect(cfg.UAAConfig().ClientSecret).To(Equal("aabbcc"))
+			Expect(cfg.UAAConfig().UaaEndpoint).To(Equal("https://uaa.cf.example.org"))
+			Expect(cfg.UAAConfig().SkipVerification).To(BeTrue())
+		})
 	})
 
-	It("returns a valid config frm the enviornment", func() {
-		c, err := NewConfig()
-
-		Expect(err).NotTo(HaveOccurred())
-		Expect(c.NatsServers).To(HaveLen(1))
-
-		natsServer := c.NatsServers[0]
-		Expect(natsServer.Host).To(Equal("10.0.1.8:4222"))
-		Expect(natsServer.User).To(Equal("nats"))
-		Expect(natsServer.Password).To(Equal("natspass"))
-	})
-
-
-	It("returns an error when nats server is not set", func() {
-		os.Setenv("ROUTESYNC_NATS_SERVERS", "[]")
-		cfg, err := NewConfig()
-		Expect(err).To(HaveOccurred())
-		Expect(err.Error()).To(ContainSubstring("no NATS servers"))
-		Expect(cfg).To(BeNil())
-	})
-
-	It("returns an error when nats server is not a JSON object", func() {
-		os.Setenv("ROUTESYNC_NATS_SERVERS", "")
-		cfg, err := NewConfig()
-		Expect(err).To(HaveOccurred())
-		Expect(err.Error()).To(ContainSubstring("JSON"))
-		Expect(cfg).To(BeNil())
-	})
-
-	It("Produces a UAA config", func() {
-		cfg, _ := NewConfig()
-		Expect(cfg.UAAConfig().ClientName).To(Equal("routeUser"))
-		Expect(cfg.UAAConfig().ClientSecret).To(Equal("aabbcc"))
-		Expect(cfg.UAAConfig().UaaEndpoint).To(Equal("https://uaa.cf.example.org"))
-		Expect(cfg.UAAConfig().SkipVerification).To(BeTrue())
+	Context("Configuration is not set", func() {
+		It("fails", func() {
+			_, err := NewConfig()
+			Expect(err).To(HaveOccurred())
+		})
 	})
 })
