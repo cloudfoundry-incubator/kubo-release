7957ed9f95931b3ea7e9483c2c67e65f5fbd35b9
Oleksandr Slynko <oslynko@pivotal.io>
2017-03-30 12:02:51 +0100

    Default source builder
    [#137357741]
    
    Signed-off-by: Konstantin Semenov <ksemenov@pivotal.io>

--- src/route-sync/application/application_configurer.go
+++ src/route-sync/application/application_configurer.go
@@ -4,7 +4,6 @@ import (
 	"route-sync/cloudfoundry"
 	"route-sync/cloudfoundry/tcp"
 	"route-sync/config"
-	"route-sync/kubernetes"
 	"route-sync/route"
 
 	uaa "code.cloudfoundry.org/uaa-go-client"
@@ -12,11 +11,6 @@ import (
 	"code.cloudfoundry.org/lager"
 	"code.cloudfoundry.org/route-registrar/messagebus"
 )
-
-func GetKubernetesSource(cfg *config.Config, logger lager.Logger, strategy kubernetes.SourceBuildStrategy) route.Source {
-	srcBuilder := kubernetes.NewSourceBuilder(logger, strategy)
-	return srcBuilder.CreateSource(cfg)
-}
 
 func GetCloudFoundryRouter(cfg *config.Config, logger lager.Logger) route.Router {
 	routerBuilder := cloudfoundry.NewCloudFoundryRoutingBuilder(cfg, logger)
