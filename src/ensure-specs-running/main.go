72adb8d40fd379e37904611cbfa6df3a2ff9106d
Amit Kumar Gupta <amitkgupta84@gmail.com>
2017-07-10 13:14:23 -0700

    src/ensure-specs-running/main.go: add logging, go fmt

--- src/ensure-specs-running/main.go
+++ src/ensure-specs-running/main.go
@@ -23,28 +23,38 @@ func main() {
 		dns:       k8sdeploy.NewDeployment(1, []string{"kubedns", "dnsmasq", "sidecar"}),
 	}
 
-    client, err := k8sadapter.DefaultClient("/var/vcap/jobs/kubeconfig/config/kubeconfig")
-    if err != nil {
-        log.Fatal(err.Error())
-    }
-    adapter := k8sadapter.NewAdapter(client, "kube-system")
+	log.Println("creating kubernetes client")
+	client, err := k8sadapter.DefaultClient("/var/vcap/jobs/kubeconfig/config/kubeconfig")
+	if err != nil {
+		log.Println("failed creating kubernetes client")
+		log.Fatalln(err.Error())
+	}
+	log.Println("succeeded creating kubernetes client")
+	adapter := k8sadapter.NewAdapter(client, "kube-system")
 
+	log.Println("fetching pods")
 	pods, err := adapter.Pods()
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Println("failed fetching pods")
+		log.Fatalln(err.Error())
 	}
+	log.Printf("succeeded fetching %d pods\n", len(pods))
 
+	log.Println("extracting deployment names for pods")
 	actualDeployments := k8sdeploy.DeploymentSet{}
 	for _, pod := range pods {
 		if deploymentName, err := adapter.ExtractDeploymentName(pod); err != nil {
-			log.Fatal(err.Error())
+			log.Printf("failed extracting deployment name for pod %s\n", pod.ObjectMeta.Name)
+			log.Fatalln(err.Error())
 		} else {
 			actualDeployments[deploymentName].AddPod(pod)
 		}
 	}
+	log.Println("succeeded extracting deployment names for pods")
 
 	discrepancies := k8sdeploy.Discrepancies(expectedDeployments, actualDeployments)
 	if len(discrepancies) != 0 {
-		log.Fatal("discrepancies found:\n- " + strings.Join(discrepancies, "\n- "))
+		log.Fatalln("discrepancies found:\n- " + strings.Join(discrepancies, "\n- "))
 	}
+	log.Println("no discrepancies found")
 }
