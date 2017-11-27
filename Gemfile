65a980697a72c2ef81279aca355379975488a8ab
Ben Chapman <bchapman@pivotal.io>
2017-11-27 16:49:42 +0000

    Release spec test suite
    [#152853461]
    
    Signed-off-by: Konstantin Semenov <ksemenov@pivotal.io>

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'bosh-template'
+gem 'rspec'
+gem 'rubocop'
---
+++ spec/kubernetes_system_specs_spec.rb
@@ -0,0 +1,58 @@
+require 'rspec'
+require 'spec_helper'
+
+
+describe 'kubernetes-system-specs' do
+  let(:link_spec) {{}}
+  let(:rendered_template) do
+    properties = {
+        'admin-password' => '1234',
+    }
+    links = link_spec
+
+    compiled_template('kubernetes-system-specs', 'bin/deploy-specs', properties, links)
+  end
+
+  it 'does not apply the standard storage class by default' do
+      expect(rendered_template).to_not include('apply_spec "storage-class-gce.yml"')
+  end
+
+  context 'on GCE' do
+    let(:link_spec) do
+      {
+          'cloud-provider' => {
+              'instances' => [],
+              'properties' => {
+                  'cloud-provider' => {
+                      'type' => 'gce'
+                  }
+              }
+          }
+      }
+    end
+
+    it 'applies the standard storage class' do
+      expect(rendered_template).to include('apply_spec "storage-class-gce.yml"')
+    end
+  end
+
+  context 'on non-GCE' do
+    let(:link_spec) do
+      {
+          'cloud-provider' => {
+              'instances' => [],
+              'properties' => {
+                  'cloud-provider' => {
+                      'type' => 'anything'
+                  }
+              }
+          }
+      }
+    end
+
+    it 'does not apply the standard storage class' do
+      expect(rendered_template).to_not include('apply_spec "storage-class-gce.yml"')
+    end
+  end
+
+end
