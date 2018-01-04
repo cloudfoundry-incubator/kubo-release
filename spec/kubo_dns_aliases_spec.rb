0f301bf257ab36bad1df9af11b600b4a12cb7f3b
Oleksandr Slynko <oslynko@pivotal.io>
2018-01-04 14:23:24 +0000

    Run rubocop --auto-correct on source code

--- spec/kubo_dns_aliases_spec.rb
+++ spec/kubo_dns_aliases_spec.rb
@@ -1,42 +1,44 @@
+# frozen_string_literal: true
+
 require 'rspec'
 require 'spec_helper'
 
 describe 'kubo-dns-aliases' do
   let(:link_spec) do
-      {
-        'kube-apiserver' => {
-          'address' => 'fake.kube-api-address',
-          'instances' => []
-        },
-        'etcd' => {
-          'address' => 'fake-etcd-address',
-          'properties' =>  { 'etcd' => { 'advertise_urls_dns_suffix' => 'dns-suffix'} },
-          'instances' => [
-            {
-             'name' => 'etcd',
-             'index' => 0,
-             'address' => 'fake-etcd-address-0',
-           },
-           {
-             'name' => 'etcd',
-             'index' => 1,
-             'address' => 'fake-etcd-address-1',
-           }
-          ]
-        }
+    {
+      'kube-apiserver' => {
+        'address' => 'fake.kube-api-address',
+        'instances' => []
+      },
+      'etcd' => {
+        'address' => 'fake-etcd-address',
+        'properties' =>  { 'etcd' => { 'advertise_urls_dns_suffix' => 'dns-suffix' } },
+        'instances' => [
+          {
+            'name' => 'etcd',
+            'index' => 0,
+            'address' => 'fake-etcd-address-0'
+          },
+          {
+            'name' => 'etcd',
+            'index' => 1,
+            'address' => 'fake-etcd-address-1'
+          }
+        ]
       }
+    }
   end
   let(:properties) { {} }
   let(:rendered_template) { compiled_template('kubo-dns-aliases', 'dns/aliases.json', properties, link_spec) }
   let(:aliases) { JSON.parse(rendered_template) }
 
   it 'aliases.json is rendered without error' do
-    expect{ JSON.parse(rendered_template) }.to_not raise_error
+    expect { JSON.parse(rendered_template) }.to_not raise_error
   end
 
   context 'master node' do
     it 'generates an alias for the master node' do
-      expect(aliases.has_key?('master.cfcr.internal')).to be(true)
+      expect(aliases.key?('master.cfcr.internal')).to be(true)
     end
 
     it 'sets the master node alias to be the wildcard dns name' do
@@ -50,8 +52,8 @@ describe 'kubo-dns-aliases' do
     let(:expected_node_prefix) { "#{etcd_instance['name']}-#{etcd_instance['index']}" }
 
     it 'generates aliases for the etcd nodes' do
-      expect(aliases.has_key?(expected_dns_suffix)).to be(true)
-      expect(aliases.has_key?("#{expected_node_prefix}.#{expected_dns_suffix}")).to be(true)
+      expect(aliases.key?(expected_dns_suffix)).to be(true)
+      expect(aliases.key?("#{expected_node_prefix}.#{expected_dns_suffix}")).to be(true)
     end
 
     it 'sets the global etcd alias to the array of etcd instances' do
--- spec/service_keys_spec.rb
+++ spec/service_keys_spec.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 require 'rspec'
 
 describe 'service key' do
-  let(:valid_json) { {'valid-json' => true}.to_json }
+  let(:valid_json) { { 'valid-json' => true }.to_json }
   let(:rendered_template) do
     properties = {
-        'cloud-provider' => {
-            'gce' => {
-                'service_key' => valid_json
-            }
+      'cloud-provider' => {
+        'gce' => {
+          'service_key' => valid_json
         }
+      }
     }
     compiled_template('cloud-provider', 'config/service_key.json', properties)
   end
