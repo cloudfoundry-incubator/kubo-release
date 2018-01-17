21285873ad8c625c2ee91106f2f2f361929fec9b
Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>
2018-01-17 16:29:10 +0100

    Add an rspec test for k8s dashboard
    [#154215217]
    
    Signed-off-by: Brenda Chan <brchan@pivotal.io>

--- spec/kubernetes_system_specs_spec.rb
+++ spec/kubernetes_system_specs_spec.rb
@@ -70,4 +70,50 @@ describe 'kubernetes-system-specs' do
       expect(rendered_template).to_not include('apply_spec "storage-class-gce.yml"')
     end
   end
+
+  context 'kubernetes-dashboard yaml' do
+    context 'if authorization mode is set to abac' do
+      let(:rendered_template) do
+        properties = {
+          'authorization-mode' => 'abac'
+        }
+
+        compiled_template('kubernetes-system-specs', 'config/kubernetes-dashboard.yml', properties)
+      end
+
+      it 'should include the kubconfig' do
+        str = '- --kubeconfig=/var/vcap/jobs/kubelet/config/kubeconfig'
+
+        expect(rendered_template).to include(str)
+      end
+
+      it 'should include the mountPath of kubconfig' do
+        str = '- mountPath: /var/vcap/jobs/kubelet/config/'
+
+        expect(rendered_template).to include(str)
+      end
+    end
+
+    context 'if authorization mode is set to rbac' do
+      let(:rendered_template) do
+        properties = {
+          'authorization-mode' => 'rbac'
+        }
+
+        compiled_template('kubernetes-system-specs', 'config/kubernetes-dashboard.yml', properties)
+      end
+
+      it 'should include a service account' do
+        str = <<~FOO
+          kind: ServiceAccount
+          metadata:
+            labels:
+              k8s-app: kubernetes-dashboard
+            name: kubernetes-dashboard
+            namespace: kube-system
+        FOO
+        expect(rendered_template).to include(str)
+      end
+    end
+  end
 end
