a3d6546372aed7e2b6eec76e77b60b7f65aba134
Sagar Muchhal <muchhals@vmware.com>
2018-01-22 14:20:02 -0800

    Moved chmod product_serial to ctl scripts
    - Created a kubo-common package to host the utils file having common
    shell functions.
    
    [#154379906]
    
    Signed-off-by: Travis Hall <thall@vmware.com>

--- src/kubo-common/utils.sh
+++ src/kubo-common/utils.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-set -exu -o pipefail
+#!/bin/bash -exu
 
 detect_cloud_config() {
   if [ -e /var/vcap/jobs/cloud-provider/bin/cloud-provider_utils ]; then
@@ -13,12 +11,9 @@ detect_cloud_config() {
   fi
 }
 
-main() {
-  detect_cloud_config
-
-  if [ "${cloud_provider}" != "" ] && [ -f /sys/class/dmi/id/product_serial ]; then
+check_and_chmod_product_serial() {
+  local cloud_provider = $1
+  if [ "$cloud_provider" != "" ] && [ -f /sys/class/dmi/id/product_serial ]; then
     chmod a+r /sys/class/dmi/id/product_serial
   fi
 }
-
-main
