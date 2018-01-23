bb456e2d58fc8c5951dce7bcc822d5829f5042ca
Sagar Muchhal <muchhalsagar88@gmail.com>
2018-01-22 23:09:49 -0800

    Fixed error in kubo-common utils script
    [#154379906]

--- src/kubo-common/utils.sh
+++ src/kubo-common/utils.sh
@@ -12,7 +12,7 @@ detect_cloud_config() {
 }
 
 check_and_chmod_product_serial() {
-  local cloud_provider = $1
+  local cloud_provider=$1
   if [ "$cloud_provider" != "" ] && [ -f /sys/class/dmi/id/product_serial ]; then
     chmod a+r /sys/class/dmi/id/product_serial
   fi
