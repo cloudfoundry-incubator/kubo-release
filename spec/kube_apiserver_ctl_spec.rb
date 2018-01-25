182b459f616a9510b266780e22c39a4d36063b2c
Ben Chapman <bchapman@pivotal.io>
2018-01-25 10:05:53 +0000

    Enabled audit logging for kube-apiserver
    [#154051305]
    
    Signed-off-by: Greg Patricio <gpatricio@pivotal.io>

---
+++ spec/kube_apiserver_ctl_spec.rb
@@ -0,0 +1,78 @@
+require 'rspec'
+require 'spec_helper'
+require 'yaml'
+
+describe 'kube-apiserver' do
+
+  let(:link_spec) do
+    {
+        'kube-apiserver' => {
+            'address' => 'fake.kube-api-address',
+            'instances' => []
+        },
+        'etcd' => {
+            'address' => 'fake-etcd-address',
+            'properties' => {'etcd' => {'advertise_urls_dns_suffix' => 'dns-suffix'}},
+            'instances' => [
+                {
+                    'name' => 'etcd',
+                    'index' => 0,
+                    'address' => 'fake-etcd-address-0'
+                },
+                {
+                    'name' => 'etcd',
+                    'index' => 1,
+                    'address' => 'fake-etcd-address-1'
+                }
+            ]
+        }
+    }
+  end
+
+
+  it 'configures audit logging by default' do
+    rendered_kube_apiserver_ctl = compiled_template(
+        'kube-apiserver',
+        'bin/kube_apiserver_ctl',
+        {},
+        link_spec
+    )
+
+    expect(rendered_kube_apiserver_ctl).to include('--audit-log-path="$AUDIT_LOG_FILE"')
+    expect(rendered_kube_apiserver_ctl).to include('--audit-log-maxage=0')
+    expect(rendered_kube_apiserver_ctl).to include('--audit-log-maxsize=0')
+    expect(rendered_kube_apiserver_ctl).to include('--audit-log-maxbackup=0')
+    expect(rendered_kube_apiserver_ctl).to include('--audit-policy-file="$CONFIG_DIR/audit_policy.yml"')
+  end
+
+  it 'does not configure audit logging when enable audit logs is false' do
+    rendered_kube_apiserver_ctl = compiled_template(
+        'kube-apiserver',
+        'bin/kube_apiserver_ctl',
+        {'enable_audit_logs' => false},
+        link_spec
+    )
+
+    expect(rendered_kube_apiserver_ctl).to_not include('--audit-log-path="$AUDIT_LOG_FILE"')
+    expect(rendered_kube_apiserver_ctl).to_not include('--audit-log-maxage=0')
+    expect(rendered_kube_apiserver_ctl).to_not include('--audit-log-maxsize=0')
+    expect(rendered_kube_apiserver_ctl).to_not include('--audit-log-maxbackup=0')
+    expect(rendered_kube_apiserver_ctl).to_not include('--audit-policy-file="$CONFIG_DIR/audit_policy.yml"')
+  end
+
+  it 'configures audit logging when enable audit logs is true' do
+    rendered_kube_apiserver_ctl = compiled_template(
+        'kube-apiserver',
+        'bin/kube_apiserver_ctl',
+        {'enable_audit_logs' => true},
+        link_spec
+    )
+
+    expect(rendered_kube_apiserver_ctl).to include('--audit-log-path="$AUDIT_LOG_FILE"')
+    expect(rendered_kube_apiserver_ctl).to include('--audit-log-maxage=0')
+    expect(rendered_kube_apiserver_ctl).to include('--audit-log-maxsize=0')
+    expect(rendered_kube_apiserver_ctl).to include('--audit-log-maxbackup=0')
+    expect(rendered_kube_apiserver_ctl).to include('--audit-policy-file="$CONFIG_DIR/audit_policy.yml"')
+  end
+
+end
