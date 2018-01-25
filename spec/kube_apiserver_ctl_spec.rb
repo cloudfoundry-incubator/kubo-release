9b8e31f55879f8e864d6d9546b93ecb6c820af6a
Greg Patricio <gpatricio@pivotal.io>
2018-01-25 10:24:49 +0000

    Fix linter errors for rspec tests
    [#154051305]
    
    Signed-off-by: Ben Chapman <bchapman@pivotal.io>

--- spec/kube_apiserver_ctl_spec.rb
+++ spec/kube_apiserver_ctl_spec.rb
@@ -1,41 +1,41 @@
+# frozen_string_literal: true
+
 require 'rspec'
 require 'spec_helper'
 require 'yaml'
 
 describe 'kube-apiserver' do
-
   let(:link_spec) do
     {
-        'kube-apiserver' => {
-            'address' => 'fake.kube-api-address',
-            'instances' => []
-        },
-        'etcd' => {
-            'address' => 'fake-etcd-address',
-            'properties' => {'etcd' => {'advertise_urls_dns_suffix' => 'dns-suffix'}},
-            'instances' => [
-                {
-                    'name' => 'etcd',
-                    'index' => 0,
-                    'address' => 'fake-etcd-address-0'
-                },
-                {
-                    'name' => 'etcd',
-                    'index' => 1,
-                    'address' => 'fake-etcd-address-1'
-                }
-            ]
-        }
+      'kube-apiserver' => {
+        'address' => 'fake.kube-api-address',
+        'instances' => []
+      },
+      'etcd' => {
+        'address' => 'fake-etcd-address',
+        'properties' => { 'etcd' => { 'advertise_urls_dns_suffix' => 'dns-suffix' } },
+        'instances' => [
+          {
+            'name' => 'etcd',
+            'index' => 0,
+            'address' => 'fake-etcd-address-0'
+          },
+          {
+            'name' => 'etcd',
+            'index' => 1,
+            'address' => 'fake-etcd-address-1'
+          }
+        ]
+      }
     }
   end
 
-
   it 'configures audit logging by default' do
     rendered_kube_apiserver_ctl = compiled_template(
-        'kube-apiserver',
-        'bin/kube_apiserver_ctl',
-        {},
-        link_spec
+      'kube-apiserver',
+      'bin/kube_apiserver_ctl',
+      {},
+      link_spec
     )
 
     expect(rendered_kube_apiserver_ctl).to include('--audit-log-path="$AUDIT_LOG_FILE"')
@@ -47,10 +47,10 @@ describe 'kube-apiserver' do
 
   it 'does not configure audit logging when enable audit logs is false' do
     rendered_kube_apiserver_ctl = compiled_template(
-        'kube-apiserver',
-        'bin/kube_apiserver_ctl',
-        {'enable_audit_logs' => false},
-        link_spec
+      'kube-apiserver',
+      'bin/kube_apiserver_ctl',
+      { 'enable_audit_logs' => false },
+      link_spec
     )
 
     expect(rendered_kube_apiserver_ctl).to_not include('--audit-log-path="$AUDIT_LOG_FILE"')
@@ -62,10 +62,10 @@ describe 'kube-apiserver' do
 
   it 'configures audit logging when enable audit logs is true' do
     rendered_kube_apiserver_ctl = compiled_template(
-        'kube-apiserver',
-        'bin/kube_apiserver_ctl',
-        {'enable_audit_logs' => true},
-        link_spec
+      'kube-apiserver',
+      'bin/kube_apiserver_ctl',
+      { 'enable_audit_logs' => true },
+      link_spec
     )
 
     expect(rendered_kube_apiserver_ctl).to include('--audit-log-path="$AUDIT_LOG_FILE"')
@@ -74,5 +74,4 @@ describe 'kube-apiserver' do
     expect(rendered_kube_apiserver_ctl).to include('--audit-log-maxbackup=0')
     expect(rendered_kube_apiserver_ctl).to include('--audit-policy-file="$CONFIG_DIR/audit_policy.yml"')
   end
-
 end
