96c04cbd5af4e85c2521741ade9183239112d187
Konstantin Semenov <ksemenov@pivotal.io>
2018-01-31 10:54:57 +0000

    Fix the template render spec
    [#154661250]
    
    Signed-off-by: Morena De Liddo <mdeliddo@pivotal.io>

--- spec/kubelet_ctl_spec.rb
+++ spec/kubelet_ctl_spec.rb
@@ -7,6 +7,6 @@ describe 'kubelet_ctl' do
   let(:rendered_template) { compiled_template('kubelet', 'bin/kubelet_ctl', {}, {}, {}, 'z1') }
 
   it 'labels the kubelet with its own az' do
-    expect(rendered_template).to include(',failure-domain.beta.kubernetes.io/zone="z1"')
+    expect(rendered_template).to include(',failure-domain.beta.kubernetes.io/zone=z1')
   end
 end
