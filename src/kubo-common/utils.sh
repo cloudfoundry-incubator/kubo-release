beb5cae88d3a764625a5ea10f819a42fb1d23c49
Greg Patricio <gpatricio@pivotal.io>
2018-02-14 11:08:29 +0000

    Share cloud-provider configuration through BOSH links
    This change updates the cloud-provider to be consumed as a BOSH link.
    Currently jobs will template these properties into their own templates,
    but the template files are shared using symlinks.
    
    This change allows us the ability to complete the conversion of the
    kube-apiserver and kube-controller-manager to BPM as the jobs no longer
    depend on templates from the `cloud-provider`.
    
    [#153690804]
    
    Signed-off-by: James Myers <jfmyers9@gmail.com>

--- src/kubo-common/utils.sh
+++ src/kubo-common/utils.sh
@@ -1,16 +1,5 @@
 #!/bin/bash -exu
 
-detect_cloud_config() {
-  if [ -e /var/vcap/jobs/cloud-provider/bin/cloud-provider_utils ]; then
-    source /var/vcap/jobs/cloud-provider/bin/cloud-provider_utils
-    set_cloud_provider
-    cloud_config="/var/vcap/jobs/cloud-provider/config/cloud-provider.ini"
-  else
-    cloud_config=""
-    cloud_provider=""
-  fi
-}
-
 check_and_chmod_product_serial() {
   local cloud_provider=$1
   if [ "$cloud_provider" != "" ] && [ -f /sys/class/dmi/id/product_serial ]; then
