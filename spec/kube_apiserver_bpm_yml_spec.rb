9f39d3289ad041069858b72a44df83b3ba9611fa
Brenda Chan <brchan@pivotal.io>
2018-04-27 15:34:46 -0400

    Allow operators to enable/disable features of a cluster
    Signed-off-by: Dave Protasowski <dprotaso@gmail.com>

--- spec/kube_apiserver_bpm_yml_spec.rb
+++ spec/kube_apiserver_bpm_yml_spec.rb
@@ -166,4 +166,21 @@ describe 'kube-apiserver' do
     expect(bpm_yml['processes'][0]['env']['no_proxy']).to eq('noproxy.example.com,noproxy.example.net')
     expect(bpm_yml['processes'][0]['env']['NO_PROXY']).to eq('noproxy.example.com,noproxy.example.net')
   end
+
+  it 'sets feature gates if the property is defined' do
+    rendered_kube_apiserver_bpm_yml = compiled_template(
+      'kube-apiserver',
+      'config/bpm.yml',
+      {
+        'feature_gates' => {
+          'CustomFeature1' => true,
+          'CustomFeature2' => false
+        }
+      },
+      link_spec
+    )
+
+    bpm_yml = YAML.safe_load(rendered_kube_apiserver_bpm_yml)
+    expect(bpm_yml['processes'][0]['args']).to include('--feature-gates=CustomFeature1=true,CustomFeature2=false')
+  end
 end
