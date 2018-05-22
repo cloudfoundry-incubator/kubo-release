a1d36eaa281d564cf01cd77c2d0fb6b68e0c2b69
Travis Hall <thall@vmware.com>
2018-05-21 19:39:21 -0700

    Remove default value for admission-plugins
    BOSH merges the hashes for the default value with the manifest-supplied
    listing, which can be confusing.
    
    Updated tests to work with explicit admission pluggin controls.

--- spec/kube_apiserver_bpm_yml_spec.rb
+++ spec/kube_apiserver_bpm_yml_spec.rb
@@ -79,35 +79,40 @@ describe 'kube-apiserver' do
     expect(bpm_yml['processes'][0]['args']).to include('--audit-policy-file=/var/vcap/jobs/kube-apiserver/config/audit_policy.yml')
   end
 
-  it 'has no security context deny when privileged containers are enabled and deny is disabled' do
+  it 'explicitly enables only the admission controllers that are marked as true' do
     rendered_kube_apiserver_bpm_yml = compiled_template(
       'kube-apiserver',
       'config/bpm.yml',
       {
         'allow_privileged' => true,
-        'disable_deny_escalating_exec' => true
+        'admission-plugins' => {
+          'DefaultStorageClass' => true,
+          'LimitRanger' => true,
+        },
       },
       link_spec
     )
 
     bpm_yml = YAML.safe_load(rendered_kube_apiserver_bpm_yml)
-    expect(bpm_yml['processes'][0]['args']).to include(
-      '--enable-admission-plugins=LimitRanger,' \
-      'NamespaceExists,NamespaceLifecycle,ResourceQuota,' \
-      'ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook,DenyEscalatingExec'
-    )
+    expect(bpm_yml['processes'][0]['args']).to include('--enable-admission-plugins=DefaultStorageClass,LimitRanger')
   end
 
-  it 'denies security context when privileged containers are not enabled' do
+  it 'explicitly disables the admission controllers that are marked as false' do
     rendered_kube_apiserver_bpm_yml = compiled_template(
       'kube-apiserver',
       'config/bpm.yml',
-      {},
+      {
+        'allow_privileged' => true,
+        'admission-plugins' => {
+          'DenyEscalatingExec' => false,
+          'LimitRanger' => false
+        },
+      },
       link_spec
     )
 
     bpm_yml = YAML.safe_load(rendered_kube_apiserver_bpm_yml)
-    expect(bpm_yml['processes'][0]['args']).to include(match(/--enable-admission-plugins=.*,SecurityContextDeny/))
+    expect(bpm_yml['processes'][0]['args']).to include('--disable-admission-plugins=DenyEscalatingExec,LimitRanger')
   end
 
   it 'has no http proxy when no proxy is defined' do
