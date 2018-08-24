35a5c26a965402a104af3fa96fbdf4de290bb93a
Carlo Colombo <carlo.colombo@gmx.com>
2018-08-24 17:48:50 +0100

    Value ending in : break bpm yaml parsing and make kube-apiserver fail (#221)
    Kubernetes suggest values as `oidc:` in --oidc-groups-prefix and
    --oidc-username-prefix. this break when is unquoted and make
    kube-apiserver to fail to start.

--- spec/kube_apiserver_bpm_yml_spec.rb
+++ spec/kube_apiserver_bpm_yml_spec.rb
@@ -185,6 +185,24 @@ describe 'kube-apiserver' do
     expect(bpm_yml['processes'][0]['args']).to include('--feature-gates=CustomFeature1=true,CustomFeature2=false')
   end
 
+  it 'set oidc properties' do
+    rendered_kube_apiserver_bpm_yml = compiled_template(
+      'kube-apiserver',
+      'config/bpm.yml',
+      {
+        'oidc' => {
+          'username-prefix' => 'oidc:',
+          'groups-prefix' => 'oidc:'
+        }
+      },
+      link_spec
+    )
+
+    bpm_yml = YAML.safe_load(rendered_kube_apiserver_bpm_yml)
+    expect(bpm_yml['processes'][0]['args']).to include('--oidc-username-prefix=oidc:')
+    expect(bpm_yml['processes'][0]['args']).to include('--oidc-groups-prefix=oidc:')
+  end
+    
   it 'defaults authorization mode to RBAC' do
     rendered_kube_apiserver_bpm_yml = compiled_template(
       'kube-apiserver',
