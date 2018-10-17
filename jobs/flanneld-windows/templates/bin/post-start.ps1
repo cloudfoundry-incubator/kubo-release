c317e017574a99099c419c778f9828c4be56cda8
Ben Moss <bmoss@pivotal.io>
2018-10-17 12:24:10 -0400

    Wait for vxlan network to appear before fixing metadata route
    Signed-off-by: Leah Hanson <lhanson@pivotal.io>

--- jobs/flanneld-windows/templates/bin/post-start.ps1
+++ jobs/flanneld-windows/templates/bin/post-start.ps1
@@ -2,4 +2,9 @@ $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
 # add back GCP / AWS metadata server
+ while (!(Get-HNSNetwork | ? Name -Eq "vxlan0"))
+{
+    Write-Host "Waiting for overlay network to be enabled"
+    Start-Sleep -sec 1
+}
 route /p add 169.254.169.254 mask 255.255.255.255 0.0.0.0
