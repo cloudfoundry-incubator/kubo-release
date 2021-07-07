760107e52b5e3b8a5eb6851b5e25829f08229408
Neil Hickey <nhickey@vmware.com>
2021-07-07 14:44:59 -0600

    Build with timestamp
    - upload to gcs
    
    Signed-off-by: Neil Hickey <nhickey@vmware.com>

--- .github/tasks/build.sh
+++ .github/tasks/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+RELEASE_TIMESTAMP=$(date +%s)
+
 bosh create-release --name kubo \
     --sha2 \
-    --tarball="kubo-release-${RELEASE_VERSION}.tgz"
+    --tarball="kubo-release-${RELEASE_TIMESTAMP}.tgz"
